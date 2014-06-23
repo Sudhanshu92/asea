b9a52eef8566d28d92bc893e4b54635c5500e8f9
Prelang Builder <builder@prelang.com>
2014-06-23 03:49:36 +0000

    Generated model Listing with migration

---
+++ db/migrate/20140623034936_create_listings.rb
@@ -0,0 +1,12 @@
+class CreateListings < ActiveRecord::Migration
+  def change
+    create_table :listings do |t|
+      t.string :title
+      t.text :description
+      t.string :link
+      t.references :hospital, index: true
+
+      t.timestamps
+    end
+  end
+end
